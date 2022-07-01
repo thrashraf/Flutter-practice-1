2c596700b6bc9bd699048422604c45b4f1952dd3
Ashraf <zulashrafvns@gmail.com>
2022-07-01 11:51:47 +0800

    add some stuff

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,77 @@ class Card extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[850],
       appBar: AppBar(
         title: Text('ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[900],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Center(
+            child: CircleAvatar(
+              backgroundImage: AssetImage('assets/me.jpg'),
+              radius: 50,
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Text(
+            'Name',
+            style: TextStyle(color: Colors.grey[300], letterSpacing: 2),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            'Zul Ashraf',
+            style: TextStyle(
+                color: Colors.amber[400],
+                fontSize: 30,
+                fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            'Age',
+            style: TextStyle(color: Colors.grey[300], letterSpacing: 2),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            //get ashraf's year
+            (DateTime.now().year - 2001).toString(),
+            style: TextStyle(
+                color: Colors.amber[400],
+                fontSize: 30,
+                fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            children: [
+              Icon(
+                Icons.email,
+                color: Colors.grey[200],
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                'zulashrafvns@gmail.com',
+                style: TextStyle(
+                    color: Colors.grey[200], fontSize: 18, letterSpacing: 1),
+              )
+            ],
+          )
+        ]),
       ),
     );
   }
